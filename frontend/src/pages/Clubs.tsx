a8f0d02a9346d3d7f029e734266def4e9821eda2
Patryk Rossa <patryk.rossaa@gmail.com>
2023-05-16 14:21:59 +0200

    fix: added key prop in clubs list

--- frontend/src/pages/Clubs.tsx
+++ frontend/src/pages/Clubs.tsx
@@ -35,7 +35,7 @@ export const Clubs = () => {
         JSON.stringify(formState),
         {
           headers: {
-            Accept: "appliaction/json",
+            Accept: "/",
             "Content-Type": "application/json"
           }
         }
